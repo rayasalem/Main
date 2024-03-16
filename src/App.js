d1405904066ae396846a7986b541eaeccb1ceeab
rayasalem <rayasalem483@gmail.com>
2024-03-16 22:06:31 +0300

    add all  file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import SectionOne from "./component/Section1/SectionOne";
 import SectionTwo from "./component/section2/SectionTwo";
 import Footer from "./component/Footer/Footer";
+import SectionThree from "./component/Section3/SectionThree";
 function App() {
   return (
     <>
     <SectionOne/>
      <SectionTwo/>
+     <SectionThree/>
      <Footer/>
      </>
   );
--- src/component/Section3/SectionThree.jsx
+++ src/component/Section3/SectionThree.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import Navbar from '../Navbar/Navbar';
-import Capture from './Capture.png';
+import Capture from './Capture.JPG';
 import './SectionThree.css'; 
 
 const SectionThree = () => {
     return (
-        <div>
-            <Navbar />
-            <div className="section-one">
+        <div className="section-three">
+            {/* Include SectionOne and SectionTwo components here */}
+            <div className="section-three-content">
                 <img src={Capture} alt="Background" />
-                <div className="section-one-content">
+                <div className="section-three-text">
                     <h1>Introduce Your Product Quickly & Effectively</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 
-mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
-                </div>
-                <div className="section-one-buttons">
-                    <button style={{ backgroundColor: 'blue', color: 'white' }}> lll Ui kit</button>
-                    <button style={{ backgroundColor: 'white', color: 'black' }}> lll Ui kit</button>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
                 </div>
             </div>
         </div>
